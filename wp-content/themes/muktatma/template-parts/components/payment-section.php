6dc68cb43432565e47ccd343756709020fb95780
tomygl1234 <tomasgimenezlascano123@gmail.com>
2025-03-21 01:48:43 +0100

    Últimos cámbios antes de que la ía corrija algunas cosas

--- wp-content/themes/muktatma/template-parts/components/payment-section.php
+++ wp-content/themes/muktatma/template-parts/components/payment-section.php
@@ -2,26 +2,22 @@
     <div class="container">
         <div class="payment-section">
             <h4><?php the_field('payment_section_title'); ?></h4>
-            <p><?php the_field('payment_subtitle_or_brief_description'); ?></p>
+            <p class="payment-description"><?php the_field('payment_subtitle_or_brief_description'); ?></p>
             <ul>
                 <?php
                 $args = array(
                     'post_type' => 'payment_option',
                     'posts_per_page' => -1,
+                    'order' => 'ASC',
+                    'orderby' => 'menu_order'
                 );
                 $payment_options = new WP_Query($args);
                 if ($payment_options->have_posts()) :
                     while ($payment_options->have_posts()) : $payment_options->the_post(); ?>
-                        <li>
-
-                            <p><?php the_title(); ?></p>
-
-
-                            <p><?php the_field('payment_section_percent_quantity_or_prices'); ?></p>
-
-
-                            <p><?php the_content(); ?></p>
-
+                        <li class="payment-card">
+                            <p class="payment-title"><?php the_title(); ?></p>
+                            <p class="payment-percentage"><?php the_field('payment_section_percent_quantity_or_prices'); ?></p>
+                            <p class="payment-content"><?php the_content(); ?></p>
                         </li>
                     <?php endwhile;
                     wp_reset_postdata();
@@ -33,5 +29,4 @@
             </ul>
         </div>
     </div>
-
 </section>
